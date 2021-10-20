194a0294c55e56ff09435b3a2906d6cc77133c4b
Zhengchun Liu <liuzhengchun@gmail.com>
2021-10-20 11:52:26 -0500

    add logs

--- main-edge.py
+++ main-edge.py
@@ -1,6 +1,6 @@
 
-import time, queue, threading
-import torch
+import time, queue, threading, sys
+import torch, argparse, logging
 from pvaccess import Channel
 from pvaccess import PvObject
 import numpy as np 
@@ -18,6 +18,8 @@ class pvaClient:
         if torch.cuda.is_available():
             self.BraggNN = self.BraggNN.to(self.torch_dev)
         self.frames_processed = 0
+        self.base_seq_id = None
+        self.recv_frames = 0
         self.tq = queue.Queue()
         threading.Thread(target=self.frame_process, daemon=True).start()
 
@@ -37,8 +39,8 @@ class pvaClient:
             tick = time.time()
             patches, patch_ori, big_peaks = frame2patch(frame=frame, psz=self.psz)
             if patches.shape[0] == 0:
-                print("%.3f, %d peaks located in frame %d, %.3fms/frame, %d peaks are too big; %d frames processed so far" % (\
-                    time.time(), patches.shape[0], frm_id, elapse, big_peaks, self.frames_processed))
+                logging.info("%.3f, %d peaks located in frame %d, %.3fms/frame, %d peaks are too big; %d frames processed so far" % (\
+                             time.time(), patches.shape[0], frm_id, elapse, big_peaks, self.frames_processed))
             input_tensor = torch.from_numpy(patches[:, np.newaxis].astype('float32'))
             # todo, infer in a batch fashion in case of out-of-memory
             with torch.no_grad():
@@ -46,17 +48,19 @@ class pvaClient:
             peak_locs, big_peaks = pred * self.psz + patch_ori, big_peaks
 
             elapse = 1000 * (time.time() - tick)
-            print("%.3f, %d peaks located in frame %d, %.3fms/frame, %d peaks are too big; %d frames processed so far" % (\
-                time.time(), peak_locs.shape[0], frm_id, elapse, big_peaks, self.frames_processed))
+            logging.info("%.3f, %d peaks located in frame %d, %.3fms/frame, %d peaks are too big; %d frames processed so far" % (\
+                         time.time(), peak_locs.shape[0], frm_id, elapse, big_peaks, self.frames_processed))
 
     def monitor(self, pv):
         uid = pv['uniqueId']
+        if self.base_seq_id is None: self.base_seq_id = uid
+        self.recv_frames += 1
         self.tq.put(pv.copy())
-        print("%.3f received frame %d, processed frames: %d" % (time.time(), uid, self.frames_processed))
+        logging.info("%.3f received frame %d, total frame received: %d, should have received: %d" % (time.time(), uid, self.recv_frames, uid - self.base_seq_id + 1))
 
-def main_monitor():
-    c = Channel('pvapy:image')
-    c.setMonitorMaxQueueLength(2000)
+def main_monitor(ch):
+    c = Channel(ch)
+    c.setMonitorMaxQueueLength(-1)
 
     client = pvaClient()
 
@@ -69,24 +73,36 @@ def main_monitor():
     c.unsubscribe('monitor')
 
 # limitation here: the same frame will be get() over and over again till server got the notification
-def main_get():
-    max_queue_size = -1
-    c = Channel('pvapy:image')
-    c.setMonitorMaxQueueLength(max_queue_size)
+# def main_get():
+#     max_queue_size = -1
+#     c = Channel('pvapy:image')
+##    c = Channel('13SIM1:Pva1:Image')
+#     c.setMonitorMaxQueueLength(max_queue_size)
 
-    while True:
-        pv = c.get('')
-        uid = pv['uniqueId']
-        print("received frame %d @ %.3f" % (uid, time.time()))
-        dims = pv['dimension']
-        rows = dims[0]['size']
-        cols = dims[1]['size']
-        frame = pv['value'][0]['ushortValue'].reshape((rows, cols))
+#     while True:
+#         pv = c.get('')
+#         uid = pv['uniqueId']
+#         print("received frame %d @ %.3f" % (uid, time.time()))
+#         dims = pv['dimension']
+#         rows = dims[0]['size']
+#         cols = dims[1]['size']
+#         frame = pv['value'][0]['ushortValue'].reshape((rows, cols))
 
-        print(frame.shape)
+#         print(frame.shape)
 
 
 if __name__ == '__main__':
-    main_monitor()
+    parser = argparse.ArgumentParser(description='edge pipeline for Bragg peak finding')
+    parser.add_argument('-ch', type=str, default='pvapy:image',    help='pva channel name')
+
+    args, unparsed = parser.parse_known_args()
+    if len(unparsed) > 0:
+        print('Unrecognized argument(s): \n%s \nProgram exiting ... ... ' % '\n'.join(unparsed))
+        exit(0)
+
+    logging.basicConfig(filename='edgeBragg.log', level=logging.DEBUG)
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+    main_monitor(args.ch)
     # main_get()
 
