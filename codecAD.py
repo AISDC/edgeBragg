ca831f9bfc84ca04bb941edc6db4f8555c4302a6
Z. Liu <zhengchun.liu@anl.gov>
2021-11-04 02:13:11 -0400

    add support for decode

---
+++ codecAD.py
@@ -0,0 +1,181 @@
+# codecAD.py
+"""
+codecAD provides python access to the codec support provided by areaDetector/ADSupport
+It is meant for use by a callback from an NTNDArray record.
+
+In order to use this code environment variable LD_LIBRARY_PATH must be defined for codec lib.
+This file was modified from 
+https://github.com/areaDetector/ADViewers/blob/master/Python/PY_NTNDA_Viewer/codecAD.py
+"""
+
+import sys
+import ctypes
+import ctypes.util
+import os
+import numpy as np
+
+
+class CodecAD:
+    def __init__(self):
+        self.__codecName = "none"
+        self.__data = None
+        self.__compressRatio = 1.0
+        self.__saveLibrary = dict()
+
+    def __findLibrary(self, name):
+        lib = self.__saveLibrary.get(name)
+        if lib != None:
+            return lib
+        result = ctypes.util.find_library(name)
+        if result == None:
+            return None
+        if os.name == "nt":
+            lib = ctypes.windll.LoadLibrary(result)
+        else:
+            lib = ctypes.cdll.LoadLibrary(result)
+        if lib != None:
+            self.__saveLibrary.update({name: lib})
+        return lib
+
+    def getCodecName(self):
+        """
+        Returns
+        -------
+        codecName : str
+            codecName for the last decompress
+        """
+        return self.__codecName
+
+    def getData(self):
+        """
+        Returns
+        -------
+        data : numpy array
+            data for the last decompress
+        """
+        return self.__data
+
+    def getCompressRatio(self):
+        """
+        Returns
+        -------
+        compressRatio : float
+            compressRatio for the last decompress
+        """
+        return self.__compressRatio
+
+    def decompress(self, data, codec, compressed, uncompressed):
+        """
+        decompress data described by codec.
+        The arguments are all provided by a callback from an NTNDArray record.
+        areaDetector/ADCore defines the NTNDArray record.
+
+        Parameters
+        ----------
+            data :        Provided by the NTNDArray record.
+            codec:        Provided by the NTNDArray record.
+            compressed:   Provided by the NTNDArray record.
+            uncompressed: Provided by the NTNDArray record.
+
+        Returns
+        -------
+            False
+                No decompression was done.
+                The original data is not modified.
+            True
+                decompression was done.
+                getCodecName, getData, and getCompressRatio provide the results
+        """
+        self.__codecName = codec["name"]
+        if len(self.__codecName) == 0:
+            self.__data = None
+            self.__codecName = "none"
+            self.__compressRatio = 1.0
+            return False
+        typevalue = codec["parameters"][0]['value']
+        if typevalue == 1:
+            dtype = "int8"
+            elementsize = int(1)
+        elif typevalue == 5:
+            dtype = "uint8"
+            elementsize = int(1)
+        elif typevalue == 2:
+            dtype = "int16"
+            elementsize = int(2)
+        elif typevalue == 6:
+            dtype = "uint16"
+            elementsize = int(2)
+        elif typevalue == 3:
+            dtype = "int32"
+            elementsize = int(4)
+        elif typevalue == 7:
+            dtype = "uint32"
+            elementsize = int(4)
+        elif typevalue == 4:
+            dtype = "int64"
+            elementsize = int(8)
+        elif typevalue == 8:
+            dtype = "uint64"
+            elementsize = int(8)
+        elif typevalue == 9:
+            dtype = "float32"
+            elementsize = int(4)
+        elif typevalue == 10:
+            dtype = "float64"
+            elementsize = int(8)
+        else:
+            raise Exception("decompress mapIntToType failed")
+        if self.__codecName == "blosc":
+            lib = self.__findLibrary(self.__codecName)
+        elif self.__codecName == "jpeg":
+            lib = self.__findLibrary("decompressJPEG")
+        elif self.__codecName == "lz4" or self.__codecName == "bslz4":
+            lib = self.__findLibrary("bitshuffle")
+        else:
+            lib = None
+        if lib == None:
+            raise Exception("shared library " + self.__codecName + " not found")
+        inarray = bytearray(data)
+        in_char_array = ctypes.c_ubyte * compressed
+        out_char_array = ctypes.c_ubyte * uncompressed
+        outarray = bytearray(uncompressed)
+        if self.__codecName == "blosc":
+            lib.blosc_decompress(
+                in_char_array.from_buffer(inarray),
+                out_char_array.from_buffer(outarray),
+                uncompressed,
+            )
+            data = np.array(outarray)
+            data = np.frombuffer(data, dtype=dtype)
+        elif self.__codecName == "lz4":
+            lib.LZ4_decompress_fast(
+                in_char_array.from_buffer(inarray),
+                out_char_array.from_buffer(outarray),
+                uncompressed,
+            )
+            data = np.array(outarray)
+            data = np.frombuffer(data, dtype=dtype)
+        elif self.__codecName == "bslz4":
+            lib.bshuf_decompress_lz4(
+                in_char_array.from_buffer(inarray),
+                out_char_array.from_buffer(outarray),
+                int(uncompressed / elementsize),
+                elementsize,
+                int(0),
+            )
+            data = np.array(outarray)
+            data = np.frombuffer(data, dtype=dtype)
+        elif self.__codecName == "jpeg":
+            lib.decompressJPEG(
+                in_char_array.from_buffer(inarray),
+                compressed,
+                out_char_array.from_buffer(outarray),
+                uncompressed,
+            )
+            data = np.array(outarray)
+            data = data.flatten()
+        else:
+            raise Exception(self.__codecName + " is unsupported codec")
+        self.__compressRatio = uncompressed / compressed
+        self.__data = data
+        return True
---
+++ edgeBraggTorchCodec.py
@@ -0,0 +1,134 @@
+
+import time, queue, threading, sys, os
+import torch, argparse, logging
+from pvaccess import Channel
+from pvaccess import PvObject
+import numpy as np 
+
+from BraggNN import BraggNN
+from frameProcess import frame_peak_patches_cv2 as frame2patch
+from codecAD import CodecAD
+
+class pvaClient:
+    def __init__(self, nth=1):
+        self.psz = 15
+        self.torch_dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.codecAD = CodecAD()
+        self.BraggNN  = BraggNN(imgsz=self.psz, fcsz=(16, 8, 4, 2)) # should use the same argu as it in the training.
+        mdl_fn = 'models/fc16_8_4_2-sz15.pth'
+        self.BraggNN .load_state_dict(torch.load(mdl_fn, map_location=torch.device('cpu')))
+        if torch.cuda.is_available():
+            self.BraggNN = self.BraggNN.to(self.torch_dev)
+        self.frames_processed = 0
+        self.base_seq_id = None
+        self.recv_frames = 0
+        self.tq = queue.Queue(maxsize=-1)
+        self.thr_exit = 0
+
+        for _ in range(nth):
+            threading.Thread(target=self.frame_process, daemon=True).start()
+
+    def frame_process(self, ):
+        while self.thr_exit == 0:
+            try:
+                pv = self.tq.get(block=True, timeout=1)
+            except queue.Empty:
+                continue
+            except:
+                logging.error("Something else of the Queue went wrong")
+                continue
+
+            frm_id= pv['uniqueId']
+            data_codec = pv['value'][0]['ubyteValue'] # will broken for uncoded, non-ubyte data
+            compressed = pv["compressedSize"]
+            uncompressed = pv["uncompressedSize"]
+            codec = pv["codec"]
+            dec_tick = time.time()
+            if self.codecAD.decompress(data_codec, codec, compressed, uncompressed):
+                data = self.codecAD.getData()
+                dec_time = 1000*(time.time() - dec_tick)
+                logging.info("[%.3f] frame %d has been decoded in %.2f ms, compress ratio is %.1f" % (\
+                             time.time(), frm_id, dec_time, self.codecAD.getCompressRatio()))
+            else:
+                logging.error("data is not compressed!")
+                data = pv['value'][0]['ushortValue']
+
+            dims  = pv['dimension']
+            rows  = dims[0]['size']
+            cols  = dims[1]['size']
+            frame = data.reshape((rows, cols))
+            self.tq.task_done()
+
+            tick = time.time()
+            patches, patch_ori, big_peaks = frame2patch(frame=frame, psz=self.psz)
+            if patches.shape[0] > 0:
+                input_tensor = torch.from_numpy(patches[:, np.newaxis].astype('float32'))
+                # todo, infer in a batch fashion in case of out-of-memory
+                with torch.no_grad():
+                    pred = self.BraggNN.forward(input_tensor.to(self.torch_dev)).cpu().numpy()
+                peak_locs, big_peaks = pred * self.psz + patch_ori, big_peaks
+
+            self.frames_processed += 1 # has race condition
+            elapse = 1000 * (time.time() - tick)
+            logging.info("[%.3f] %d peaks located in frame %d, %.3fms/frame, %d peaks are too big; %d out of %d frames processed so far" % (\
+                        time.time(), patches.shape[0], frm_id, elapse, big_peaks, self.frames_processed, self.recv_frames))
+
+        logging.info(f"worker thread {threading.get_ident() } exiting now")
+
+    def monitor(self, pv):
+        uid = pv['uniqueId']
+        if self.base_seq_id is None: self.base_seq_id = uid
+        self.recv_frames += 1
+        self.tq.put(pv.copy())
+        logging.info("[%.3f] received frame %d, total frame received: %d, should have received: %d; %d frames pending process" % (\
+                     time.time(), uid, self.recv_frames, uid - self.base_seq_id + 1, self.tq.qsize()))
+
+def main_monitor(ch, nth):
+    c = Channel(ch)
+    c.setMonitorMaxQueueLength(-1)
+
+    client = pvaClient(nth)
+
+    c.subscribe('monitor', client.monitor)
+    c.startMonitor('')
+
+    # ToDo check if it is done from server/detector, where streaming gives signal
+    time.sleep(2)
+
+    # exit when idle for 10 seconds
+    while True:
+        recv_prog = client.recv_frames
+        proc_prog = client.frames_processed
+        time.sleep(10)
+        if recv_prog == client.recv_frames and proc_prog == client.frames_processed:
+            logging.info("program exits because of silence")
+            break
+    client.tq.join()
+    client.thr_exit = 1
+    time.sleep(2) # give threads seconds to exit
+
+    c.stopMonitor()
+    c.unsubscribe('monitor')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='edge pipeline for Bragg peak finding')
+    parser.add_argument('-gpus', type=str, default="0", help='list of visiable GPUs')
+    parser.add_argument('-ch',   type=str, default='13SIM1:Pva1:Image', help='pva channel name')
+    parser.add_argument('-nth',  type=int, default=1, help='number of threads for frame processes')
+    parser.add_argument('-terminal',  type=int, default=1, help='non-zero to print logs to stdout')
+
+    args, unparsed = parser.parse_known_args()
+    if len(unparsed) > 0:
+        print('Unrecognized argument(s): \n%s \nProgram exiting ... ... ' % '\n'.join(unparsed))
+        exit(0)
+
+    if len(args.gpus) > 0:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
+
+    logging.basicConfig(filename='edgeBragg.log', level=logging.DEBUG,\
+                        format='%(asctime)s %(levelname)-8s %(message)s',)
+    if args.terminal != 0:
+        logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+    main_monitor(ch=args.ch, nth=args.nth)
+
